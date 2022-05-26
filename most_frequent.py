9a575a08380c1d377b3ad58743d4eafe5c74bc01
Kanishak Chaurasia <kanishakchaurasia2@gmail.com>
2022-05-26 07:19:47 +0530

    bug fixed

--- most_frequent.py
+++ most_frequent.py
@@ -4,28 +4,26 @@ frequent_word = ""
 frequency = 0 
 words = []
  
-# Traversing file line by line
+
 for line in file:
      
-    # splits each line into
-    # words and removing spaces
-    # and punctuations from the input
+
     line_word = line.lower().replace(',','').replace('.','').split(" "); 
      
-    # Adding them to list words
+   
     for w in line_word: 
-        words.append(w); 
+        words.append(w)
          
-# Finding the max occurred word
+
 for i in range(0, len(words)): 
      
     # Declaring count
-    count = 1; 
+    count = 1
      
     # Count each word in the file 
     for j in range(i+1, len(words)): 
         if(words[i] == words[j]): 
-            count = count + 1; 
+            count = count + 1
  
     # If the count value is more
     # than highest frequency then
@@ -35,4 +33,4 @@ for i in range(0, len(words)):
  
 print("Most repeated word: " + frequent_word)
 print("Frequency: " + str(frequency))
-file.close();
+file.close()
